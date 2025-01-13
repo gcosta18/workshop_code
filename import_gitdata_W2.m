fefcbfdbc5d0d1ea01a2691b25979c5c7baa258f
gcosta18 <gcosta18@gmail.com>
2025-01-13 21:40:16 +0000

    update

---
+++ import_gitdata_W2.m
@@ -0,0 +1,17 @@
+%% Week 2
+
+% load data from github
+url = 'https://github.com/gcosta18/workshop_data/raw/refs/heads/main/spike_train_data_w2t1.mat';
+filename = 'spike_train_data_w2t1.mat';
+websave(filename, url);
+
+% load data from github
+url = 'https://github.com/gcosta18/workshop_data/raw/refs/heads/main/spike_train_time_w2t1.mat';
+filename = 'spike_train_time_w2t1.mat';
+websave(filename, url);
+
+
+% load tutorial file from github
+url = 'https://github.com/gcosta18/workshop_code/raw/refs/heads/main/W2T1.mlx';
+filename = 'W2T1.mlx';
+websave(filename, url);
