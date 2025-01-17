c1a0e3a8c7995b2b87c1024ec8f5b8282307b7ca
gcosta18 <gcosta18@gmail.com>
2025-01-17 16:29:26 +0000

    update with week 1 and 2
    - includes verified tutorials and their solutions

--- import_gitdata_W2.m
+++ import_gitdata_W2.m
@@ -16,6 +16,12 @@ url = 'https://github.com/gcosta18/workshop_code/raw/refs/heads/main/W2T1.mlx';
 filename = 'W2T1.mlx';
 websave(filename, url);
 
+
+% load tutorial solutions file from github
+url = 'https://github.com/gcosta18/workshop_code/raw/refs/heads/main/W2T1_solutions.mlx';
+filename = 'W2T1_solutions.mlx';
+websave(filename, url);
+
 % load tutorial file from github
 url = 'https://github.com/gcosta18/workshop_code/raw/refs/heads/main/warm_up.mlx';
 filename = 'warm_up.mlx';
