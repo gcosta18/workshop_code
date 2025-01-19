962c30abab539dbcc8ac7a029fcef72230101fd8
gcosta18 <37438537+gcosta18@users.noreply.github.com>
2025-01-19 23:53:09 +0000

    W3_assignments

---
+++ import_gitdata_W3.m
@@ -0,0 +1,13 @@
+%% Week 3
+
+% load data from github
+url = 'https://raw.githubusercontent.com/gcosta18/workshop_data/refs/heads/main/headbrain.csv';
+filename = 'headbrain.csv';
+websave(filename, url);
+
+
+% load tutorial file from github
+url = 'https://github.com/gcosta18/workshop_code/raw/refs/heads/main/W3T1.mlx';
+filename = 'W3T1.mlx';
+websave(filename, url);
+
