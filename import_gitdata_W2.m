237f4e158a6538c85823fdfd0bc3a8d73cb75631
gcosta18 <37438537+gcosta18@users.noreply.github.com>
2025-01-20 10:56:44 +0000

    update with W2T2.mlx

--- import_gitdata_W2.m
+++ import_gitdata_W2.m
@@ -16,10 +16,14 @@ url = 'https://github.com/gcosta18/workshop_code/raw/refs/heads/main/W2T1.mlx';
 filename = 'W2T1.mlx';
 websave(filename, url);
 
-
 % load tutorial solutions file from github
 url = 'https://github.com/gcosta18/workshop_code/raw/refs/heads/main/W2T1_solutions.mlx';
 filename = 'W2T1_solutions.mlx';
+websave(filename, url);
+
+% load tutorial file from github
+url = 'https://github.com/gcosta18/workshop_code/raw/refs/heads/main/W2T2.mlx';
+filename = 'W2T2.mlx';
 websave(filename, url);
 
 % load tutorial file from github
