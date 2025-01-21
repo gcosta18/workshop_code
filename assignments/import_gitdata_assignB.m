c2c7c1ec5cdc87c400f5bc3758d534138aa51c60
gcosta18 <37438537+gcosta18@users.noreply.github.com>
2025-01-21 13:20:57 +0000

    update assignment

---
+++ assignments/import_gitdata_assignB.m
@@ -0,0 +1,19 @@
+%% Assignment
+
+% load data from github
+url = 'https://github.com/gcosta18/workshop_data/raw/refs/heads/main/sptimes_b.mat';
+filename = 'sptimes.mat';
+websave(filename, url);
+
+
+
+% load tutorial file from github
+url = 'https://github.com/gcosta18/workshop_code/raw/refs/heads/main/W2T2_assign_b.mlx';
+filename = 'W2T2_assign.mlx';
+websave(filename, url);
+
+
+% load tutorial file from github
+url = 'https://github.com/gcosta18/workshop_code/raw/refs/heads/main/warm_up.mlx';
+filename = 'warm_up.mlx';
+websave(filename, url);
