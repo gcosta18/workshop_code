645e7b80a4fd298f781b3577c03f76b9413f05b0
gcosta18 <gcosta18@gmail.com>
2025-04-10 12:35:25 +0100

    updates

--- import_gitPortfolio_odd.m
+++ import_gitPortfolio_odd.m
@@ -1,18 +1,18 @@
 %% Module PGU33016 - Portfolio Part 2
 % for MATLAB ONLINE
 
-% load data from github
-url = 'https://github.com/gcosta18/workshop_data/raw/refs/heads/main/warm_up.mlx';
-filename = 'warm_up.mat';
-websave(filename, url);
 
 % load data from github
 url = 'https://github.com/gcosta18/workshop_data/raw/refs/heads/main/WHO_statsA.mat';
 filename = 'WHO_stats.mat';
 websave(filename, url);
 
-
 % load tutorial file from github
+url = 'https://github.com/gcosta18/workshop_code/raw/refs/heads/main/warm_up.mlx';
+filename = 'warm_up.mlx';
+websave(filename, url);
+
+% load assignment from github
 url = 'https://github.com/gcosta18/workshop_code/raw/refs/heads/main/SECTION_A_odd.mlx';
 filename = 'SECTION_A.mlx';
 websave(filename, url);
